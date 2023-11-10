8acdb8489ff1d58c6a3a8a716d0151d1033a9790
Pranav <pranav.techiegeek@gmail.com>
2023-11-10 20:50:51 +0530

    ready

--- script.py
+++ script.py
@@ -1,16 +1,9 @@
-def calculate_bmi(height, weight):
-    """
-    Calculate the Body Mass Index (BMI) and determine the BMI category.
-    
-    :param height: Height in centimeters.
-    :param weight: Weight in kilograms.
-    :return: A tuple containing the BMI value (rounded to two decimal places) and the BMI category.
-    """
+def is_palindrome(s):
     pass
-    
 
 # Example function calls
-print(calculate_bmi(155, 43.3))  # Expected: (18.02, "underweight")
-print(calculate_bmi(170, 62.2))  # Expected: (21.52, "healthy")
-print(calculate_bmi(150, 77.1))  # Expected: (34.27, "obese")
-print(calculate_bmi(189, 101.2)) # Expected: (28.37, "overweight")
+print(is_palindrome("racecar"))  # Should return True
+print(is_palindrome("codedamn"))  # Should return False
+print(is_palindrome(""))         # Should return True
+print(is_palindrome("A man, a plan, a canal: Panama"))  # Should return True
+print(is_palindrome("No lemon, no melon"))              # Should return True
