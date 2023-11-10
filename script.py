b8cfc8465f9a375de32cd225364e95fedf91b0cc
Pranav <pranav.techiegeek@gmail.com>
2023-11-10 16:20:00 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+def sum_of_n(n):
+    """
+    Calculates the sum of all integers from 1 to n.
+
+    Args:
+    n (int): A positive integer up to which the sum is calculated.
+
+    Returns:
+    int: The sum of all integers from 1 to n.
+    """
+    # Handle the edge case where n is 0
+    
+
+    # Calculate the sum of numbers from 1 to n
+    
+    pass
+
+# Test cases
+print(sum_of_n(10))  # Expected output: 55
+print(sum_of_n(25))  # Expected output: 325
+print(sum_of_n(0))   # Expected output: 0
+print(sum_of_n(100)) # Expected output: 5050
