35c8d2cfae1c9e3480eea99a4529c93d8b967143
Pranav <pranav.techiegeek@gmail.com>
2023-11-10 16:53:44 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+def reverse_string(s):
+    """
+    Reverses a given string.
+
+    :param s: The string to be reversed.
+    :return: The reversed string.
+    """
+    pass
+
+# Sample Tests
+print("hello =>", reverse_string("hello"))  # Expected Output: olleh
+print("codedamn =>", reverse_string("codedamn"))  # Expected Output: nmadedoc
+print("racecar =>", reverse_string("racecar"))  # Expected Output: racecar
+print("12345 =>", reverse_string("12345"))  # Expected Output: 54321
