b3398a4766ccbe4b11ece7d42b673503d5f3a337
Pranav <pranav.techiegeek@gmail.com>
2023-11-12 04:35:26 +0530

    ready

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
-def is_palindrome(s):
+def is_prime_number(n):
+    """
+    Check if the provided number n is a prime number.
+    Returns True if n is prime, and False otherwise.
+    """
     pass
 
-# Example function calls
-print(is_palindrome("racecar"))  # Should return True
-print(is_palindrome("codedamn"))  # Should return False
-print(is_palindrome(""))         # Should return True
-print(is_palindrome("A man, a plan, a canal: Panama"))  # Should return True
-print(is_palindrome("No lemon, no melon"))              # Should return True
+# Sample Function Calls for testing - Hit the run button to see the output
+print(f"is_prime_number(3): {is_prime_number(3)}")  # Expected: True
+print(f"is_prime_number(41): {is_prime_number(41)}")  # Expected: True
+print(f"is_prime_number(6): {is_prime_number(6)}")  # Expected: False
+print(f"is_prime_number(1): {is_prime_number(1)}")  # Expected: False
+print(f"is_prime_number(-3): {is_prime_number(-3)}")  # Expected: False
+print(f"is_prime_number(1.1): {is_prime_number(1.1)}")  # Expected: False
+
