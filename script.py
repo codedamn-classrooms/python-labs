5c791b591935dc2aee2e0a60ac3f80adb5ed9573
Pranav <pranav.techiegeek@gmail.com>
2023-11-12 17:21:21 +0530

    ready

--- script.py
+++ script.py
@@ -1,15 +1,21 @@
-def is_prime_number(n):
+def convert_distance(distance, unit):
     """
-    Check if the provided number n is a prime number.
-    Returns True if n is prime, and False otherwise.
+    Converts a distance between kilometers and meters.
+
+    Parameters:
+    - distance (float): The distance to be converted.
+    - unit (str): The unit of the distance ('km' or 'mtr').
+
+    Returns:
+    - tuple: A tuple containing the converted distance and its unit.
     """
     pass
 
-# Sample Function Calls for testing - Hit the run button to see the output
-print(f"is_prime_number(3): {is_prime_number(3)}")  # Expected: True
-print(f"is_prime_number(41): {is_prime_number(41)}")  # Expected: True
-print(f"is_prime_number(6): {is_prime_number(6)}")  # Expected: False
-print(f"is_prime_number(1): {is_prime_number(1)}")  # Expected: False
-print(f"is_prime_number(-3): {is_prime_number(-3)}")  # Expected: False
-print(f"is_prime_number(1.1): {is_prime_number(1.1)}")  # Expected: False
+# Example function calls
+print(convert_distance(1.5, "km"))  # Expected output: (1500.0, "mtr")
+print(convert_distance(3000, "mtr"))  # Expected output: (3.0, "km")
+print(convert_distance(0, "mtr"))  # Expected output: (0.0, "km")
 
+# NOTE: If you're testing an error case, make sure to comment the function call before executing the tests
+# Sample Error Case 
+# print(convert_distance(-2, "mtr"))  # Expected output: ValueError
