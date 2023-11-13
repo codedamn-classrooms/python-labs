5f7e165e8ee3498495d41c85e57e250d92481902
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 21:12:37 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+def factorial(n):
+    """
+    Calculate the factorial of a given positive integer n using recursion.
+    """
+    # Base case: if n is 1, return 1 (since 1! = 1)
+    pass
+
+# Test cases
+print("Factorial of 5:", factorial(5))   # Expected: 120
+print("Factorial of 10:", factorial(10))  # Expected: 3628800
+print("Factorial of 8:", factorial(8))   # Expected: 40320
+print("Factorial of 18:", factorial(18))  # Expected: 6402373705728000
