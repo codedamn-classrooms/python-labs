f9c91a0f08dc330637b7253f07a079fdcc66de43
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 20:33:41 +0530

    ready

--- script.py
+++ script.py
@@ -4,5 +4,5 @@ def generate_password():
 
 # Test cases
 print(generate_password(length=8, letters_count=3, numbers_count=3, symbols_count=2))
-print(generate_password(length=6))
+print(generate_password)
 print(generate_password(length=10, letters_count=4, numbers_count=3, symbols_count=3))
