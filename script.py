04b165a48cf618659b1ecc678c425dd8319dad74
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 18:50:45 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+def fibonacci_numbers(N):
+    """
+    Return a tuple containing the (N-1)th, Nth, and (N+1)th Fibonacci numbers.
+    For N <= 0, it handles edge cases as specified.
+    """
+    pass
+
+# Example function calls
+print(fibonacci_numbers(5))  # Expected: (3, 5, 8)
+print(fibonacci_numbers(0))  # Expected: (-1, -1, 0)
+print(fibonacci_numbers(-1)) # Expected: (-1, -1, -1)
+print(fibonacci_numbers(4))  # Expected: (1, 2, 3)
+print(fibonacci_numbers(1))  # Expected: (-1, 0, 1)
