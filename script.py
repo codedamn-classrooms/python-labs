8cec3bd906fbf2be32a5e2f8d44b85a300844a64
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 18:39:08 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+def is_power_of_two(number):
+    pass
+
+# Test cases as examples
+print("is_power_of_two(4):", is_power_of_two(4))  # Should return True
+print("is_power_of_two(6):", is_power_of_two(6))  # Should return False
+print("is_power_of_two(8):", is_power_of_two(8))  # Should return True
+print("is_power_of_two(7):", is_power_of_two(7))  # Should return False
+print("is_power_of_two(1):", is_power_of_two(1))  # Should return True (2^0)
+print("is_power_of_two(0):", is_power_of_two(0))  # Should return False (Edge case)
