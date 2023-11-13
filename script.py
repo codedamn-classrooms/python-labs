6e4e4e909f62042f909e7a1b99821f28eb861802
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 17:49:05 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+def is_anagram(str1, str2):
+    """
+    Determine if the two strings are anagrams, considering case insensitivity and ignoring non-letter characters.
+    """
+    pass
+
+# Example function calls
+print(is_anagram("Listen", "Silent"))  # Expected output: True
+print(is_anagram("Hello", "World"))    # Expected output: False
+print(is_anagram("Dormitory", "Dirty room"))  # Expected output: True
