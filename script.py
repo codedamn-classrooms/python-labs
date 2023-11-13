918dab4d6350625c9343a93649dd5978677f03f5
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 17:44:20 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+def square_numbers(numbers):
+    """
+    Squares each number in the given list.
+    Integers are squared normally.
+    Floating point numbers are squared and rounded to two decimal places.
+    An empty list returns an empty list.
+    """
+    pass
+
+# Example function calls and printing the output
+print(square_numbers([2, 3]))  # Should return [4, 9]
+print(square_numbers([-2, 2.3]))  # Should return [4, 5.29]
+print(square_numbers([]))  # Should return []
