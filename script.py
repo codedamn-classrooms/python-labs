b4eee15cf20b78eb07bec8fa4fe6073681070d40
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 18:27:21 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+def central_tendency(data):
+    """
+    Calculate the mean, median, and mode of a list of numbers.
+    
+    Parameters:
+    data (list): A list of numerical data.
+
+    Returns:
+    dict: A dictionary containing the mean, median, and mode.
+    """
+    pass
+
+# Example function calls
+print("Example 1:", central_tendency([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
+print("Example 2:", central_tendency([5, 7, 12, 15, 21, 23, 23, 30, 34, 36, 40, 45, 50, 55]))
+print("Example 3:", central_tendency([1, 1, 2, 3, 4, 5, 8, 13]))
