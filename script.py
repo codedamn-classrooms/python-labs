867553ecbc6547a57af734c30e42eda9379bee2f
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 17:55:12 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import string
+
+def count_words(text):
+    pass
+
+# Example function calls
+print(count_words("Hello world! Hello again, world."))  # Should return {"hello": 2, "world": 2, "again": 1}
+print(count_words("Unique words only once."))  # Should return {"unique": 1, "words": 1, "only": 1, "once": 1}
