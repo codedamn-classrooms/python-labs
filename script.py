900e1c2c4b9a15e85cc838b5446c758700e18593
Pranav <pranav.techiegeek@gmail.com>
2023-11-14 12:20:36 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+def sum_of_digits(n):
+    """
+    This function takes an integer n, sums its digits, and continues summing the digits
+    of the result until a single-digit number is obtained. This single-digit result is then returned.
+    """
+    pass
+
+# Example function calls and printing their outputs
+if __name__ == "__main__":
+    examples = [29, 12345, 45, 7, 987654, 0]
+    for example in examples:
+        print(f"sum_of_digits({example}) = {sum_of_digits(example)}")
