f1ff819daaba5116868a845cd24287b2488c9442
Pranav <pranav.techiegeek@gmail.com>
2023-11-14 13:20:51 +0530

    ready

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+def list_even_numbers(n):
+    """
+    Generate a list of even numbers from 1 to n (inclusive) using list comprehension.
+
+    Parameters:
+    n (int): The upper limit of the range
+
+    Returns:
+    list: A list of even numbers within the specified range
+    """
+    pass
+
+# Example function calls
+print("Output for n=5:", list_even_numbers(5))
+print("Output for n=30:", list_even_numbers(30))
+print("Output for n=15:", list_even_numbers(14))
