bdf24ed0c246fe4bfac3c619580933a91f87c06e
Pranav <pranav.techiegeek@gmail.com>
2023-11-15 10:30:44 +0530

    ready

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ def is_palindrome(s):
 # Example function calls
 print(is_palindrome("racecar"))  # Should return True
 print(is_palindrome("codedamn"))  # Should return False
-print(is_palindrome(""))         # Should return True
+print(is_palindrome(""))         # Should return None
 print(is_palindrome("A man, a plan, a canal: Panama"))  # Should return True
 print(is_palindrome("No lemon, no melon"))              # Should return True
